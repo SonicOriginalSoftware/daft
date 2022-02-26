e91302da2ca8adff11cdf4ed117aa1b5aacaf07e
Nathan Blair <me@nathanblair.rocks>
2022-02-26 01:05:38 -0500

    still wip
    need sleep

--- conductor/lib/main.go
+++ conductor/lib/main.go
@@ -1,6 +1,7 @@
 package lib
 
 import (
+	"conductor/generated"
 	"fmt"
 	"log"
 	"net"
@@ -11,11 +12,13 @@ import (
 
 var port = lookupPort()
 
-// RegisterFunction is a function that registers a grpc.ServiceRegistrar and a ProtoServer
-type RegisterFunction func(s grpc.ServiceRegistrar, srv ProtoServer)
+type Service generated.RunnerServer
+
+// RegisterFunction is a function that registers a grpc.ServiceRegistrar and a Server Service
+type RegisterFunction func(s grpc.ServiceRegistrar, server Service)
 
 // Main wraps the main functionality of all servers
-func Main(registerFunction RegisterFunction, service ProtoServer) {
+func Main(registerFunction RegisterFunction, service Service) {
 	outlog := log.New(os.Stdout, "", log.LstdFlags)
 	errlog := log.New(os.Stderr, "[ERROR] ", log.LstdFlags)
 
